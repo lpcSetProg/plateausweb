08c3e829d76c9d07918fa75d1de29bb529a8243d
Lev Cocarell <levyprog@Levs-MacBook-Pro.local>
2019-05-12 12:43:53 -0400

    added beta button

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,9 +1,1 @@
-export const DB_CONFIG  = {
-    apiKey: "AIzaSyD6ujM9oWK1PjTz_fEuZ_AlIhiKJhAb_Xw",
-    authDomain: "plateaus-web.firebaseapp.com",
-    databaseURL: "https://plateaus-web.firebaseio.com",
-    projectId: "plateaus-web",
-    storageBucket: "plateaus-web.appspot.com",
-    messagingSenderId: "1024491145675",
-    appId: "1:1024491145675:web:5555d2164a5038b4"
-  };
+
